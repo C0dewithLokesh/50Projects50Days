e488952a0a651ad12aae4f48e79f1b6c55dfc4a7
Lokesh <77185999+C0dewithLokesh@users.noreply.github.com>
2023-03-12 23:51:17 +0530

    Movie App

--- Movie App/script.js
+++ Movie App/script.js
@@ -1,6 +1,6 @@
 const API_URL = 'https://api.themoviedb.org/3/discover/movie?sort_by=popularity.desc&api_key=23b4538d54810c111b2469c65f6eb114&page=1'
 const IMG_PATH = 'https://image.tmdb.org/t/p/w1280'
-const SEARCH_API = 'https://api.themoviedb.org/3/search/movie?api_key=23b4538d54810c111b2469c65f6eb114&query="'
+const SEARCH_API = 'https://api.themoviedb.org/3/search/movie?api_key=23b4538d54810c111b2469c65f6eb114&query='
 
 const main = document.getElementById("main")
 const form = document.getElementById("form")
